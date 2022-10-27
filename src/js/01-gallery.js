2467f3f186dfffbe630565f41320a5b16f6ed55b
Yuliia Cherkashyna <jul11091109@gmail.com>
2022-10-27 23:35:09 +0300

    7

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const paletteContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryItemsCardMarkup(galleryItems);
+paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+paletteContainer.addEventListener('click', onPaletteContainerClick);
+
+
+
+function createGalleryItemsCardMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+            <div class="gallery__item">
+                <a class="gallery__link" "${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </div>
+           ` ;
+        }).join('');
+}
+
+function onPaletteContainerClick(evt) {
+    evt.preventDefault()
+    const iceGalleryEl = evt.target.classList.contains('gallery__image');
+    if (!iceGalleryEl) {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+    
+`)
+ 
+console.log(evt.target.dataset.source)
+    instance.show()
+    
+    document.addEventListener("keydown", onEscapeGallery)
+
+function onEscapeGallery (event) {
+    if (event.key === 'Escape') {
+      instance.close();
+  }
+}
+  
+}
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,48 @@
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const paletteContainer = document.querySelector('.gallery');
+console.log(paletteContainer)
+const cardsMarkup = createGalleryItemsCardMarkup(galleryItems);
+paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+// paletteContainer.addEventListener('click', onPaletteContainerClick);
+
+function createGalleryItemsCardMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+            <a class="gallery__item" "${original}">
+               <img 
+                   class="gallery__image" 
+                   src="${preview}" 
+                   alt="${description}" 
+                   />
+            </a>
+            ` ;
+        }).join('')
+   
+};
+
+// function onPaletteContainerClick(evt) {
+//     evt.preventDefault()
+//     const iceGalleryEl = evt.target.classList.contains('gallery__image');
+//     if (!iceGalleryEl) {
+//         return;
+//     }
+
+    // const instance = basicLightbox.create(`
+    // <img src="${evt.target.dataset.source}" width="800" height="600">
+    
+// `)
+
+ 
+    // console.log(iceGalleryEl)
+    // instance.show()
+// }
+
+
+
+new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
