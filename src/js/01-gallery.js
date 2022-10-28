c5ac58d1afcad00c233a448ac301b57f2e9dced9
Yuliia Cherkashyna <jul11091109@gmail.com>
2022-10-28 13:17:39 +0300

    д.з7 друге завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,17 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+console.log(galleryItems);
 const paletteContainer = document.querySelector('.gallery');
 const cardsMarkup = createGalleryItemsCardMarkup(galleryItems);
-paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+paletteContainer.insertAdjacentHTML('afterbegin', cardsMarkup);
 paletteContainer.addEventListener('click', onPaletteContainerClick);
 
-
-
 function createGalleryItemsCardMarkup(galleryItems) {
-    return galleryItems
-        .map(({ preview, original, description }) => {
-            return `
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
             <div class="gallery__item">
                 <a class="gallery__link" "${original}">
                     <img
@@ -22,31 +20,31 @@ function createGalleryItemsCardMarkup(galleryItems) {
                     />
                 </a>
             </div>
-           ` ;
-        }).join('');
+           `;
+    })
+    .join('');
 }
 
 function onPaletteContainerClick(evt) {
-    evt.preventDefault()
-    const iceGalleryEl = evt.target.classList.contains('gallery__image');
-    if (!iceGalleryEl) {
-        return;
-    }
+  evt.preventDefault();
+  const iceGalleryEl = evt.target.classList.contains('gallery__image');
+  if (!iceGalleryEl) {
+    return;
+  }
 
-    const instance = basicLightbox.create(`
+  const instance = basicLightbox.create(`
     <img src="${evt.target.dataset.source}" width="800" height="600">
     
-`)
- 
-console.log(evt.target.dataset.source)
-    instance.show()
-    
-    document.addEventListener("keydown", onEscapeGallery)
+`);
 
-function onEscapeGallery (event) {
+  console.log(evt.target.dataset.source);
+  instance.show();
+
+  document.addEventListener('keydown', onEscapeGallery);
+
+  function onEscapeGallery(event) {
     if (event.key === 'Escape') {
       instance.close();
+    }
   }
-}
-  
 }
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,48 +1,29 @@
-
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-// console.log(galleryItems);
+console.log(galleryItems);
 
 const paletteContainer = document.querySelector('.gallery');
-console.log(paletteContainer)
+console.log(paletteContainer);
 const cardsMarkup = createGalleryItemsCardMarkup(galleryItems);
-paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
-// paletteContainer.addEventListener('click', onPaletteContainerClick);
+paletteContainer.insertAdjacentHTML('afterbegin', cardsMarkup);
 
 function createGalleryItemsCardMarkup(galleryItems) {
-    return galleryItems
-        .map(({ preview, original, description }) => {
-            return `
-            <a class="gallery__item" "${original}">
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+           
+            <a class="gallery__item" href="${original}">
                <img 
                    class="gallery__image" 
                    src="${preview}" 
                    alt="${description}" 
                    />
             </a>
-            ` ;
-        }).join('')
-   
-};
+          
+            `;
+    })
+    .join('');
+}
 
-// function onPaletteContainerClick(evt) {
-//     evt.preventDefault()
-//     const iceGalleryEl = evt.target.classList.contains('gallery__image');
-//     if (!iceGalleryEl) {
-//         return;
-//     }
-
-    // const instance = basicLightbox.create(`
-    // <img src="${evt.target.dataset.source}" width="800" height="600">
-    
-// `)
-
- 
-    // console.log(iceGalleryEl)
-    // instance.show()
-// }
-
-
-
-new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
